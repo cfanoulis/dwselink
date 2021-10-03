061383d786ad6c2235595fd59beb95333eaf6e33
Charalampos Fanoulis <yo@fanoulis.dev>
2021-10-03 19:11:22 +0300

    feat: yank useInterval react hook

---
+++ web/src/util/hooks/useInterval.ts
@@ -0,0 +1,24 @@
+// Source: https://overreacted.io/making-setinterval-declarative-with-react-hooks/. Used with permission.
+// Copyright 2016 Dan Abramov
+
+import React, { useState, useEffect, useRef } from 'react';
+
+export function useInterval(callback, delay) {
+	const savedCallback = useRef();
+
+	// Remember the latest callback.
+	useEffect(() => {
+		savedCallback.current = callback;
+	}, [callback]);
+
+	// Set up the interval.
+	useEffect(() => {
+		function tick() {
+			savedCallback.current();
+		}
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay]);
+}

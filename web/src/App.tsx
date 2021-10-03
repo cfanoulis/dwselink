772f26a32a7ba6c626b34e28a20480a994e501c0
Charalampos Fanoulis <yo@fanoulis.dev>
2021-10-03 16:17:20 +0300

    fix: yeet supabase auth far away

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,3 @@
-import { AuthProvider } from '@redwoodjs/auth';
-import { createClient } from '@supabase/supabase-js';
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web';
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo';
 
@@ -8,16 +6,12 @@ import Routes from 'src/Routes';
 
 import './index.css';
 
-const supabaseClient = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
 const App = () => (
 	<FatalErrorBoundary page={FatalErrorPage}>
 		<RedwoodProvider titleTemplate="%AppTitle">
-			<AuthProvider client={supabaseClient} type="supabase">
-				<RedwoodApolloProvider>
-					<Routes />
-				</RedwoodApolloProvider>
-			</AuthProvider>
+			<RedwoodApolloProvider>
+				<Routes />
+			</RedwoodApolloProvider>
 		</RedwoodProvider>
 	</FatalErrorBoundary>
 );

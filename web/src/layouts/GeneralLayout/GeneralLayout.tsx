456f407cdaf0d58db6c9a45a96395a79bd662458
Charalampos Fanoulis <yo@fanoulis.dev>
2021-10-12 13:56:44 +0300

    feat: align text left

--- web/src/layouts/GeneralLayout/GeneralLayout.tsx
+++ web/src/layouts/GeneralLayout/GeneralLayout.tsx
@@ -3,7 +3,11 @@ type GeneralLayoutProps = {
 };
 
 const GeneralLayout = ({ children }: GeneralLayoutProps) => {
-	return <div className="flex flex-col items-center justify-center w-screen h-screen bg-blue-50 p-2 text-center">{children}</div>;
+	return (
+		<main className="flex flex-col items-center justify-center w-screen h-screen bg-blue-50 p-2">
+			<div className="flex flex-col items-start">{children}</div>
+		</main>
+	);
 };
 
 export default GeneralLayout;
